0f4dde06cb2971f5f546ef24dead7301fbcaa219
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-08-11 07:34:34 +0700

    feat(stats): fetch statistics dynamically using '@tanstack/solid-query' and server actions
    - Refactor 'Stats.tsx' to query statistics using '@tanstack/solid-query' and 'actions.getStats'
    - Update 'Card.tsx' to render icons dynamically using 'Dynamic' and conditional suffix with 'haveX'
    - Add '@utility loader' and '@utility hover-underline-2' to 'global.css'

--- src/components/stats/Card.tsx
+++ src/components/stats/Card.tsx
@@ -1,17 +1,22 @@
 import type { JSX, Component } from "solid-js";
+import { Dynamic } from "solid-js/web";
 
 type Props = {
 	icon: Component<JSX.SvgSVGAttributes<SVGSVGElement>>;
 	stat: string;
 	name: string;
 	description: string;
+	haveX?: boolean;
 };
 
 export default function Card(props: Props) {
 	return (
-		<div class="min-block-[20.5rem] grid grid-cols-[1fr_auto] grid-rows-[1fr_auto_auto] px-6 py-6 border-border lg:border-e lg:nth-3:border-be-0 not-last-of-type:border-be">
-			<props.icon class="block-auto inline-7" />
-			<p class="text-4xl">{props.stat}</p>
+		<div class="min-block-[20.5rem] grid grid-cols-[1fr_auto] grid-rows-[1fr_auto_auto] px-6 py-6 border-border border-x md:odd:border-e-0 md:nth-3:border-be-0 lg:odd:border-s-0 not-last-of-type:border-be">
+			<Dynamic component={props.icon} class="block-auto inline-7" />
+			<p class="text-4xl">
+				{props.stat}
+				{props.haveX && <span>×</span>}
+			</p>
 			<p class="col-span-2">{props.name}</p>
 			<p class="col-span-2 text-sm">{props.description}</p>
 		</div>
--- src/components/stats/Stats.tsx
+++ src/components/stats/Stats.tsx
@@ -1,36 +1,87 @@
-import { Plus } from "lucide-solid";
+import { actions } from "astro:actions";
+import {
+	QueryClient,
+	QueryClientProvider,
+	useQuery,
+} from "@tanstack/solid-query";
+import { Switch, Match, For } from "solid-js";
+import type { JSX, Component } from "solid-js";
+import { Plus, Sparkle, ArrowRight, TrendingUp } from "lucide-solid";
+
+import { classList } from "@utils/class-list";
 
 import Card from "./Card";
 
-export default function Stats() {
+const queryClient = new QueryClient();
+
+function StatsContent() {
+	const query = useQuery(() => ({
+		queryKey: ["stats"],
+		queryFn: async () => {
+			const { data, error } = await actions.getStats();
+			if (error) {
+				throw error;
+			}
+			return data;
+		},
+	}));
+
+	// display stat number with compact notation
+	function formatNumber(num: number) {
+		return new Intl.NumberFormat("en-US", {
+			notation: "compact",
+			maximumFractionDigits: 2,
+		}).format(num);
+	}
+
+	// Icon mapping to render icon dynamically based on name
+	const iconMapping: Record<
+		string,
+		Component<JSX.SvgSVGAttributes<SVGSVGElement>>
+	> = {
+		"Students reached": Sparkle,
+		"Schools partnered": Plus,
+		"Teachers trained": ArrowRight,
+		"Graduation lift": TrendingUp,
+	};
+
 	return (
-		<div class="grid lg:grid-cols-2">
-			<Card
-				icon={Plus}
-				stat="1,284"
-				name="Schools partnered"
-				description="In 14 countries, from Kenya to Guatemala."
-			/>
-			<Card
-				icon={Plus}
-				stat="1,284"
-				name="Schools partnered"
-				description="In 14 countries, from Kenya to Guatemala."
-			/>
-			<Card
-				icon={Plus}
-				stat="1,284"
-				name="Schools partnered"
-				description="In 14 countries, from Kenya to Guatemala."
-			/>
-			<Card
-				icon={Plus}
-				stat="1,284"
-				name="Schools partnered"
-				description="In 14 countries, from Kenya to Guatemala."
-			/>
+		<div
+			class={classList(
+				"grid inline-full max-inline-[26rem] mx-auto md:max-inline-[50rem]",
+				query.isSuccess
+					? "md:grid-cols-[1fr_1fr]"
+					: "place-content-center py-8 border-border lg:border-e",
+			)}>
+			<Switch>
+				<Match when={query.isPending}>
+					<div class="loader"></div>
+				</Match>
+				<Match when={query.isError}>
+					<div class="text-red-300">{query.error?.message}</div>
+				</Match>
+				<Match when={query.isSuccess}>
+					<For each={query.data}>
+						{(item) => (
+							<Card
+								icon={iconMapping[item.name]}
+								stat={formatNumber(item.stat)}
+								name={item.name}
+								description={item.description}
+								haveX={item.name === "Graduation lift"}
+							/>
+						)}
+					</For>
+				</Match>
+			</Switch>
 		</div>
 	);
 }
 
-// fetch data dari database dan ganti placeholder
+export default function Stats() {
+	return (
+		<QueryClientProvider client={queryClient}>
+			<StatsContent />
+		</QueryClientProvider>
+	);
+}
