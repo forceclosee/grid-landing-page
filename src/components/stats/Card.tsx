574f47109f6814263dcd02dd72bdcff836dbc798
ForceClose <273080622+forceclosee@users.noreply.github.com>
2026-08-12 07:35:00 +0700

    style(stats): add transition to 'Card'
    Add transition to the 'div' element of the 'Card' component in 'Card.tsx'

--- src/components/stats/Card.tsx
+++ src/components/stats/Card.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function Card(props: Props) {
 	return (
-		<div class="min-block-[20.5rem] grid grid-cols-[1fr_auto] grid-rows-[1fr_auto_auto] hover:bg-white/5 px-6 py-6 border-border border-x md:odd:border-e-0 md:nth-3:border-be-0 lg:odd:border-s-0 not-last-of-type:border-be cursor-pointer">
+		<div class="min-block-[20.5rem] grid grid-cols-[1fr_auto] grid-rows-[1fr_auto_auto] hover:bg-white/5 px-6 py-6 border-border border-x md:odd:border-e-0 md:nth-3:border-be-0 lg:odd:border-s-0 not-last-of-type:border-be transition-all duration-200 cursor-pointer">
 			<Dynamic component={props.icon} class="block-auto inline-7" />
 			<p class="text-4xl">
 				{props.stat}
